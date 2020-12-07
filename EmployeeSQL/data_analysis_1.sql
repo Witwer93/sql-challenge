06f9e3e48cfd61a837ddb05cdeb5b376a6abb50a
Peter Witwer <pwitwer@icloud.com>
2020-12-07 16:48:44 -0700

    finished data analysis 5

--- EmployeeSQL/data_analysis_1.sql
+++ EmployeeSQL/data_analysis_1.sql
@@ -34,3 +34,13 @@ ORDER BY d.department
 ------------------------------------------------------------------------
 DATA ANALYSIS 5
 
+SELECT e.first_name, e.last_name, e.sex
+FROM employees AS e
+WHERE(
+		e.first_name = 'Hercules' 
+	AND e.last_name LIKE 'B%'
+	)
+
+------------------------------------------------------------------------
+DATA ANALYSIS 6
+
