5e9b5f3fa34220f755948a168d253e9a93f142f9
Peter Witwer <pwitwer@icloud.com>
2020-12-07 16:41:38 -0700

    finished data analysis 4

--- EmployeeSQL/data_analysis_1.sql
+++ EmployeeSQL/data_analysis_1.sql
@@ -25,3 +25,12 @@ INNER JOIN employees AS e ON m.emp_no = e.emp_no;
 ------------------------------------------------------------------------
 DATA ANALYSIS 4
 
+SELECT e.emp_no, e.last_name, e.first_name, de.dept_no, d.department
+FROM employees AS e
+INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no
+INNER JOIN departments AS d ON de.dept_no = d.dept_no
+ORDER BY d.department
+
+------------------------------------------------------------------------
+DATA ANALYSIS 5
+
